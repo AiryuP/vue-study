cf7a8fe7701df67536f9f4b6e7c649708b4554b8
pengyu <airpyu@163.com>
2019-01-07 18:09:46 +0800

    循环数组

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
+
   <div id="app">
-   
+    <h2>你好，VUE！{{ msg }}</h2>
+    <br>
+    这是一个跟组件！{{ obj.name }}
+
+    <br/>
+
+    <ul> 
+      <li v-for="a in list">
+        {{ a }}
+      </li>
+    </ul>
+
+
+    
   </div>
 </template>
 
@@ -9,7 +23,15 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      obj:{
+        name: '张三'
+      },
+      list:[
+        "111",
+        "222",
+        "333"
+      ]
     }
   }
 }
