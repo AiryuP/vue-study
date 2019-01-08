a10889f48315f7f3a7ffbe20f06592efa8025131
pengyu <airpyu@163.com>
2019-01-08 18:15:43 +0800

    添加组件（绑定属性）未完成

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,21 @@
         {{ a }}
       </li>
     </ul>
+    <br/><br/>
 
+    <div v-for="b in list1">
+        {{ b.name }} {{ b.sex }}
+    </div>
 
+    <br/><br/><br/>
+    <div>
+      <div v-for="item in list2">
+        <div> {{ item.cate }} </div>
+        <ul v-for=" ite in item.list ">
+          <li> {{ ite.name }} {{ ite.content }}</li>
+        </ul>
+      </div>
+    </div>
     
   </div>
 </template>
@@ -25,12 +38,33 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       obj:{
-        name: '张三'
+        "name": '张三'
       },
       list:[
         "111",
         "222",
         "333"
+      ],
+      list1:[
+        {"name":'张三',"sex":'男'},
+        {"name":'李四',"sex":'男'},
+        {"name":'小红',"sex":'女'}
+      ],
+      list2:[
+        {
+          "cate": '国内新闻',
+          "list":[
+            { "name":'张一山',"content": '刘星的扮演者' },
+            { "name": '杨紫',"content": '参加综艺' },
+            { "name": '张三',"content": '玩蛇'}
+          ]
+        },{
+          "cate": '国外新闻',
+          "list":[
+            { "name": '泰勒斯威夫特', "content": '发布新歌' },
+            { "name": '小李子',"content": '瘦回来了' }
+          ]
+        }
       ]
     }
   }
---
+++ src/assets/comment/bindClass.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app01">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'bindClass',
+    data(){
+        return{
+            msg: '绑定属性，绑定class，绑定style'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
